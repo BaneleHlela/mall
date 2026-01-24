6391b88fb6f191f666712922da986b04c9f360d6
BaneleHlela <gizahlela@gmail.com>
2026-01-24 17:25:23 +0200

    14 January 2025-8

--- backend/routes/payfastRoutes.js
+++ backend/routes/payfastRoutes.js
@@ -15,15 +15,19 @@ router.post("/create", async (req, res) => {
     package: "Package Payment",
   };
 
+  const FRONTEND_URL = process.env.FRONTEND_PUBLIC_URL || "https://lula-slakeless-hortatively.ngrok-free.dev";
+  const BACKEND_URL = process.env.BACKEND_URL || "https://lula-slakeless-hortatively.ngrok-free.dev";
+
+
+
   const paymentData = {
     merchant_id: process.env.PAYFAST_MERCHANT_ID,
     merchant_key: process.env.PAYFAST_MERCHANT_KEY,
 
-    return_url: `${process.env.FRONTEND_PUBLIC_URL}/payment/success` || "https://lula-slakeless-hortatively.ngrok-free.dev/payment/success",
-    cancel_url: `${process.env.FRONTEND_PUBLIC_URL}/payment/cancel` ||  "https://lula-slakeless-hortatively.ngrok-free.dev/payment/cancel",
-    notify_url: process.env.BACKEND_URL
-    ? `${process.env.BACKEND_URL}/api/payments/payfast/itn`
-    : "https://lula-slakeless-hortatively.ngrok-free.dev/api/payments/payfast/itn",
+    return_url: `${FRONTEND_URL}/payment/success`,
+    cancel_url: `${FRONTEND_URL}/payment/cancel`,
+    notify_url: `${BACKEND_URL}/api/payments/payfast/itn`,
+
 
     name_first: "Mall",
     name_last: "Hlela",
@@ -53,17 +57,17 @@ router.post("/create", async (req, res) => {
 
 router.post("/itn", express.urlencoded({ extended: false }), async (req, res) => {
   console.log("Received ITN:", req.body);
-  const data = req.body;
+  
+  const receivedData = { ...req.body }; // clone
+  const receivedSignature = receivedData.signature;
+  delete receivedData.signature;
 
-  const receivedSignature = data.signature;
-  delete data.signature;
-
-  const expectedSignature = generatePayFastSignature(data, process.env.PAYFAST_PASSPHRASE);
+  const expectedSignature = generatePayFastSignature(receivedData, process.env.PAYFAST_PASSPHRASE);
   console.log("Expected Signature:", expectedSignature);
   console.log("Received Signature:", receivedSignature);
-  console.log(expectedSignature === receivedSignature ? "Signatures match." : "Signatures do not match.");
 
   if (receivedSignature !== expectedSignature) {
+    console.error("❌ PayFast signature mismatch");
     return res.status(400).send("Invalid signature");
   }
 
