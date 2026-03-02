c90870308f9ebff96d3d9cbfae7bbef1e633fcf5
BaneleHlela <gizahlela@gmail.com>
2026-03-02 19:39:21 +0200

    02 March 2026

--- frontend/src/features/context.ts
+++ frontend/src/features/context.ts
@@ -1,3 +1,5 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://mall-backend-env.eba-gjttyizk.eu-north-1.elasticbeanstalk.com";
+export const API_URL = "https://mall-backend-4w8l.onrender.com";
 
-//import.meta.env.VITE_API_URL || 
+//export const API_URL = import.meta.env.VITE_API_URL || "http://mall-backend-env.eba-gjttyizk.eu-north-1.elasticbeanstalk.com";
+
+
