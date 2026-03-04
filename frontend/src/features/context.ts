5890a5d33ee6066e03a87f2c581984403be39093
BaneleHlela <gizahlela@gmail.com>
2026-03-04 13:12:27 +0200

    debug

--- frontend/src/features/context.ts
+++ frontend/src/features/context.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://187.124.22.218:5000" || "https://mall-backend-4w8l.onrender.com";
+export const API_URL = "https://api.themallbeta.com" || "https://mall-backend-4w8l.onrender.com";
 
 //export const API_URL = import.meta.env.VITE_API_URL || "http://mall-backend-env.eba-gjttyizk.eu-north-1.elasticbeanstalk.com";
 
