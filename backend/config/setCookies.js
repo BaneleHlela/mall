e1165d091bfa670f799c72b752905b02e5f8bd33
BaneleHlela <gizahlela@gmail.com>
2026-03-13 14:08:30 +0200

    Auth -11

--- backend/config/setCookies.js
+++ backend/config/setCookies.js
@@ -1,18 +1,20 @@
 export const setCookies = (res, accessToken, refreshToken) => {
 	const isProduction = process.env.NODE_ENV === "production";
 
+	console.log(isProduction ? "Setting cookies for production" : "Setting cookies for development");
+
 	res.cookie("accessToken", accessToken, {
 		httpOnly: true,
 		secure: isProduction,
 		sameSite: isProduction ? "none" : "lax",
 		maxAge: 15 * 60 * 1000,
-		domain: isProduction ? "www.themallbeta.com" : "localhost",
+		domain: isProduction ? "themallbeta.com" : "localhost",
 	});
 	res.cookie("refreshToken", refreshToken, {
 		httpOnly: true,
 		secure: isProduction,
 		sameSite: isProduction ? "none" : "lax",
 		maxAge: 7 * 24 * 60 * 60 * 1000,
-		domain: isProduction ? "www.themallbeta.com" : "localhost",
+		domain: isProduction ? "themallbeta.com" : "localhost",
 	});
 };
